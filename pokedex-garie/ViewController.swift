f09f045c37e38366906dd8dd6afe24b4857ce9a6
Gary Foo <gfmr90@gmail.com>
2015-11-25 21:24:31 +1000

    Initial Commit

---
+++ pokedex-garie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-garie
+//
+//  Created by Gary Foo on 25/11/2015.
+//  Copyright © 2015 Eden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
